3b5b98693e2015b2c68032dd578bd7c166f84faa
sadid56 <sadidhasan77@gmail.com>
2024-05-29 11:51:53 +0600

    change somthing

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-const { default: axios } = require('axios');
 const fs = require('fs');
-const path = require('path');
 
-async function fetchAndSaveIcons() {
+function readIconsFromJson() {
     try {
+        // Read JSON data from the public directory
         const jsonData = fs.readFileSync('./public/api.json', 'utf-8');
         const icons = JSON.parse(jsonData);
-        console.log(icons);
-
-        // Create the icons directory if it doesn't exist
-        const iconsDir = path.join(__dirname, 'public');
-        if (!fs.existsSync(iconsDir)) {
-            fs.mkdirSync(iconsDir);
-        }
-
-        icons.forEach(async icon => {
-            const { name, icon: iconUrl } = icon;
-            const imageResponse = await axios.get(iconUrl, { responseType: 'arraybuffer' });
-            fs.writeFileSync(path.join(iconsDir, `${name}.png`), imageResponse.data);
-        });
-
-        console.log('Icons fetched and saved successfully.');
+        return icons;
     } catch (error) {
-        console.error('Error fetching icons:', error);
+        console.error('Error reading icons from JSON:', error);
+        return [];
     }
 }
 
-fetchAndSaveIcons();
+module.exports = { readIconsFromJson };
---
+++ readFile.js
@@ -0,0 +1,4 @@
+const { readIconsFromJson } = require("./index");
+
+// Call the function to read icons from JSON
+readIconsFromJson();
